2634c54949b9afd30c524ebf2e4a10647014f7f7
Eunsoo Park <eunsoo.park@iconloop.com>
2023-03-13 09:59:26 +0900

    Setup source ICON network with configuration file

--- e2edemo/scripts/icon/network.ts
+++ e2edemo/scripts/icon/network.ts
@@ -1,5 +1,7 @@
 import IconService from 'icon-sdk-js';
 import Wallet from "icon-sdk-js/build/Wallet";
+import fs from "fs";
+import path from 'path';
 
 const {IconWallet, HttpProvider} = IconService;
 const {E2E_DEMO_PATH} = process.env;
@@ -8,21 +10,35 @@ export class IconNetwork {
   iconService: IconService;
   nid: number;
   wallet: Wallet;
+  preps: Wallet[];
   private static instance: IconNetwork;
 
-  constructor(_iconService: IconService, _nid: number, _wallet: Wallet) {
+  constructor(_iconService: IconService, _nid: number, _wallet: Wallet, _preps?: Wallet[]) {
     this.iconService = _iconService;
     this.nid = _nid;
     this.wallet = _wallet;
+    this.preps = _preps ? _preps : [];
   }
 
-  public static getDefault() {
+  public static getDefault(confPath: string) {
     if (!this.instance) {
-      const httpProvider = new HttpProvider('http://localhost:9080/api/v3');
+      const data = fs.readFileSync(confPath);
+      const conf = JSON.parse(data.toString());
+      const httpProvider = new HttpProvider(conf.endpoint);
       const iconService = new IconService(httpProvider);
-      const keystore = require(`${E2E_DEMO_PATH}/docker/icon/config/keystore.json`);
-      const wallet = IconWallet.loadKeystore(keystore, 'gochain', false);
-      this.instance = new this(iconService, 3, wallet);
+      const keystore = require(path.resolve(E2E_DEMO_PATH ? E2E_DEMO_PATH:"./", conf.keystore));
+      const wallet = IconWallet.loadKeystore(keystore, conf.keypass, false);
+      let preps, i
+      preps = []
+      if (conf.preps == undefined) {
+        preps.push(wallet)
+      } else {
+        for (i = 0; i < conf.preps.length; ++i) {
+          const w = IconWallet.loadPrivateKey(conf.preps[i]);
+          preps.push(w)
+        }
+      }
+      this.instance = new this(iconService, conf.nid, wallet, preps);
     }
     return this.instance;
   }
